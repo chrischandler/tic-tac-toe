579061d6d40545b43e116c89e50354dfd44a2060
Chris Chandler <chris@chrischandler.com>
2020-05-09 20:00:43 +0800

    Updates

---
+++ precache-manifest.77d43ea1a1760bd0251498aec32efc45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d433819fc5ca8d3f8efc24028db7f70f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7f1b782e1c14f4fc3aa8",
+    "url": "/tic-tac-toe/static/css/main.b1d4d55b.chunk.css"
+  },
+  {
+    "revision": "a21d82df8e6e19a064d6",
+    "url": "/tic-tac-toe/static/js/2.6873c516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.6873c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1b782e1c14f4fc3aa8",
+    "url": "/tic-tac-toe/static/js/main.8ba6748c.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
